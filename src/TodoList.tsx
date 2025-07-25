314b5804f448b7ed5b5726e9395acec75202013a
Marco Furrer <marcofurrer0@gmail.com>
2025-07-25 13:42:32 +0200

    feat: Bump version to 1.3.0 and enable comments in TodoList editor

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -323,6 +323,7 @@ const TodoList: React.FC<TodoListProps> = ({
             isOpen={isEditorOpen}
             onSave={saveFromEditor}
             onClose={closeEditor}
+            showComments={true}
           />
         )}
       </div>
